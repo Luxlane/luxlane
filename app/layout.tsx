e1b847185872f81e72c28de851428e0036c7630b
Luxlane <jc.globaladvisory@gmail.com>
2025-08-17 00:50:25 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
+
 import "./globals.css";
 import type { ReactNode } from "react";
 import Image from "next/image";
