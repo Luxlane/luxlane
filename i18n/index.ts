f185134b56f55578100e6eca201af4829255ab29
Luxlane <jc.globaladvisory@gmail.com>
2025-08-17 16:21:55 -0600

    Update index.ts

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,8 +1,6 @@
 import es from "./es";
 import en from "./en";
 
-export type Lang = "es" | "en";
-
-export function getDict(lang: Lang) {
+export function getDict(lang: "es" | "en") {
   return lang === "en" ? en : es;
 }
