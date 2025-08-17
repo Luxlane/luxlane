8c19fc2e55f5f3ae266bd94b20e8a7fdf9b7821f
Luxlane <jc.globaladvisory@gmail.com>
2025-08-17 15:45:00 -0600

    Update route.ts

--- app/lang/[code]/route.ts
+++ app/lang/[code]/route.ts
@@ -1,17 +1,18 @@
+// app/lang/[code]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { code: string } }
+  { params }: { params: { code: "es" | "en" } }
 ) {
-  const code = params.code === "en" ? "en" : "es";
-  const referer = req.headers.get("referer") ?? "/";
-  const url = new URL(referer, req.url);
+  const lang = params.code;
 
-  const res = NextResponse.redirect(url.toString());
-  res.cookies.set("lang", code, {
+  const res = NextResponse.redirect(req.headers.get("referer") || "/");
+  res.cookies.set("lang", lang, {
     path: "/",
     maxAge: 60 * 60 * 24 * 365, // 1 año
+    sameSite: "strict",
   });
+
   return res;
 }
