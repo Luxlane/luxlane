597e4acad2bf2552e526f6724c1fa8cca1223e26
Luxlane <jc.globaladvisory@gmail.com>
2025-08-17 21:01:45 -0600

    Update index.ts

--- i18n/index.ts
+++ i18n/index.ts
@@ -2,11 +2,7 @@ import es from "./es";
 import en from "./en";
 
 export type Lang = "es" | "en";
-
-const dicts = {
-  es,
-  en,
-};
+const dicts = { es, en };
 
 export function getDict(lang: Lang) {
   return dicts[lang] || dicts.es; // fallback a español
